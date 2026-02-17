0174da8a578ac925ede8bbc29d449c0f3e50713a
robiulalam02 <robiulbusiness02@gmail.com>
2026-02-18 00:12:54 +0600

    update root folder

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
